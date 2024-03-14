0a5da885c5a53e101025a4baf8d6998bb82b1bdd
Evgeny Cherednichenko <More1ggg@yandex.ru>
2024-03-14 18:40:22 +0300

    Update blogicum

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,3 @@
-from django.db.models.base import Model as Model
 from django.db.models import Count
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render, redirect
